a7e9dbb538397a53664396d56cddcda681df5d86
GitMoDu <batatas@gmail.com>
2025-07-01 13:19:25 +0100

    Moved uart client to uart library.

--- src/VirtualPadUartInterface.h
+++
@@ -1,9 +0,0 @@
-#ifndef _VIRTUAL_PAD_UART_INTERFACE_INCLUDE_h
-#define _VIRTUAL_PAD_UART_INTERFACE_INCLUDE_h
-
-
-#include "UartInterface/VirtualPadUartInterface.h"
-#include "UartInterface/VirtualPadUartServer.h"
-#include "UartInterface/VirtualPadUartClient.h"
-
-#endif
